d08b429d9bd9fcfba8fd6b11691fa6ba96317cbe
Harshul Mittal <harshul.mittal@sunfox.in>
2023-07-20 13:25:49 +0530

    Latest Update

--- src/main/java/com/example/check2/STLinkCLITest.java
+++ src/main/java/com/example/check2/STLinkCLITest.java
@@ -1,34 +1,37 @@
 package com.example.check2;
 
+import javafx.application.Platform;
 import javafx.stage.FileChooser;
 
 import java.io.*;
 
 public class STLinkCLITest {
     public static void main(String[] args) {
-        try {
-            // Command
-            // to execute
-            String command = "cmd /c cd STM32 ST-LINK Utility/ST-LINK Utility && ST-LINK_CLI.exe -P C://Users//techn//Desktop//BLINK_JAVA.bin 0x08000000 -V";
 
-            // Create ProcessBuilder
+//        try{
+//        Process process = Runtime.getRuntime().exec("cmd /c cd STM32 ST-LINK Utility/ST-LINK Utility && ST-LINK_CLI.exe -P C://Users//admin//Downloads//BLINK_JAVA.bin  0x08000000 -V");
+//        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//        String inputString;
+//        while ((inputString = stdInput.readLine()) != null) {
+//            System.out.println(inputString);
+//
+//        }
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//    }
+        try {
+            String command = "cmd /c cd STM32 ST-LINK Utility/ST-LINK Utility && ST-LINK_CLI.exe -P C://Users//admin//Downloads//BLINK_JAVA.bin 0x08000000 -V";
             ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
             processBuilder.redirectErrorStream(true);
 
-            // Start the process
             Process process = processBuilder.start();
-
-            // Get the output stream of the process
             InputStream inputStream = process.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            // Read the output
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
-
-            // Wait for the process to complete
             int exitCode = process.waitFor();
             System.out.println("Command executed with exit code: " + exitCode);
 
